7d6222b75141d3633f060217684a4e708707d2a9
Vangleidson <85074657+Vangleidson@users.noreply.github.com>
2021-06-08 22:33:59 -0300

    bug do menu horizontal arrumado

--- scripts/contato.js
+++ scripts/contato.js
@@ -1,4 +1,17 @@
+var mouseIn1 = document.getElementsByTagName("a")[0]
+mouseIn1.addEventListener("mouseenter", entrar1)
+mouseIn1.addEventListener("mouseout", sair1)
 var cor = "rgb(122, 31, 240)"
+function entrar1(){
+    mouseIn1.style.borderBottom = "solid 3px"
+    mouseIn1.style.color = cor
+}
+
+function sair1(){
+    mouseIn1.style.borderBottom = "black"
+    mouseIn1.style.color = "black"
+}
+
 var mouseIn2 = document.getElementsByTagName("a")[1]
 mouseIn2.addEventListener("mouseenter", entrar2)
 mouseIn2.addEventListener("mouseout", sair2)
@@ -13,16 +26,4 @@ function sair2(){
     mouseIn2.style.color = "black"
 }
 
-var mouseIn3 = document.getElementsByTagName("a")[2]
-mouseIn3.addEventListener("mouseenter", entrar3)
-mouseIn3.addEventListener("mouseout", sair3)
 
-function entrar3(){
-    mouseIn3.style.borderBottom = "solid 3px"
-    mouseIn3.style.color = cor
-}
-
-function sair3(){
-    mouseIn3.style.borderBottom = "black"
-    mouseIn3.style.color = "black"
-}
--- scripts/sobre.js
+++ scripts/sobre.js
@@ -1,16 +1,15 @@
+var mouseIn1 = document.getElementsByTagName("a")[0]
+mouseIn1.addEventListener("mouseenter", entrar1)
+mouseIn1.addEventListener("mouseout", sair1)
 var cor = "rgb(122, 31, 240)"
-var mouseIn2 = document.getElementsByTagName("a")[1]
-mouseIn2.addEventListener("mouseenter", entrar2)
-mouseIn2.addEventListener("mouseout", sair2)
-
-function entrar2(){
-    mouseIn2.style.borderBottom = "solid 3px"
-    mouseIn2.style.color = cor
+function entrar1(){
+    mouseIn1.style.borderBottom = "solid 3px"
+    mouseIn1.style.color = cor
 }
 
-function sair2(){
-    mouseIn2.style.borderBottom = "black"
-    mouseIn2.style.color = "black"
+function sair1(){
+    mouseIn1.style.borderBottom = "black"
+    mouseIn1.style.color = "black"
 }
 
 var mouseIn3 = document.getElementsByTagName("a")[2]
